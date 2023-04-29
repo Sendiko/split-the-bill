1053c90e747412eb7d9a5ca330e64126c8370d40
Sendiko <sarangtawon897@gmail.com>
2023-04-29 23:31:42 +0700

    enhancement - code cleanup

--- app/src/main/java/com/sendiko/split_the_bill/ui/components/TextFields.kt
+++ app/src/main/java/com/sendiko/split_the_bill/ui/components/TextFields.kt
@@ -2,19 +2,14 @@ package com.sendiko.split_the_bill.ui.components
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -52,23 +47,4 @@ fun CustomOutlinedTextFields(
             }
         }
     )
-}
-
-@Preview
-@Composable
-fun ComponentsPrev() {
-    Surface {
-        CustomOutlinedTextFields(
-            value = "",
-            onNewValue = {
-
-            },
-            hint = "Rp. 000",
-            isError = false,
-            leadingIcon = {
-                Icon(imageVector = Icons.Default.Person, contentDescription = "person")
-            },
-            errorMessage = "ajksdhfjksdfh"
-        )
-    }
 }
--- app/src/main/java/com/sendiko/split_the_bill/ui/screen/SplitBillScreen.kt
+++ app/src/main/java/com/sendiko/split_the_bill/ui/screen/SplitBillScreen.kt
@@ -9,15 +9,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.History
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -28,7 +25,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
