83609bce71e65a468a2e4d6bba74bb2142cce48d
Sendiko <sarangtawon897@gmail.com>
2023-05-01 19:10:13 +0700

    enhancement - create room db instance

---
+++ app/src/main/java/com/sendiko/split_the_bill/repository/database/Database.kt
@@ -0,0 +1,30 @@
+package com.sendiko.split_the_bill.repository.database
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.sendiko.split_the_bill.repository.models.Bills
+
+@androidx.room.Database(entities = [Bills::class], version = 1)
+abstract class Database: RoomDatabase() {
+    abstract val dao: BillDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: Database? = null
+
+        fun getDatabase(context: Context): Database {
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    Database::class.java,
+                    "splitbill.db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+    }
+
+}
