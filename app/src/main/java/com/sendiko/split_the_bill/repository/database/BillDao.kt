e67a49cf3b883787b78df459b1f12c9982665389
Sendiko <sarangtawon897@gmail.com>
2023-05-01 19:09:46 +0700

    enhancement - add room db dependency

---
+++ app/src/main/java/com/sendiko/split_the_bill/repository/database/BillDao.kt
@@ -0,0 +1,22 @@
+package com.sendiko.split_the_bill.repository.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.sendiko.split_the_bill.repository.models.Bills
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BillDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBill(bills: Bills)
+
+    @Delete
+    suspend fun deleteBill(bills: Bills)
+
+    @Query("select * from bills order by id asc")
+    fun getBills(): Flow<List<Bills>>
+}
