27bd92a692aa9337ca4736328075f63e8f8f3f7f
Sendiko <sarangtawon897@gmail.com>
2023-05-01 19:12:59 +0700

    enhancement - update screen, and show history list

--- app/src/main/java/com/sendiko/split_the_bill/ui/screen/SplitBillScreen.kt
+++ app/src/main/java/com/sendiko/split_the_bill/ui/screen/SplitBillScreen.kt
@@ -3,15 +3,19 @@ package com.sendiko.split_the_bill.ui.screen
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -21,22 +25,28 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sendiko.split_the_bill.ui.components.BillsItem
 import com.sendiko.split_the_bill.ui.components.CustomOutlinedTextFields
-import com.sendiko.split_the_bill.viewmodels.SplitBillViewModel
+import com.sendiko.split_the_bill.ui.events.SplitBillEvent
+import com.sendiko.split_the_bill.ui.state.SplitBillState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SplitBillScreen(
-    viewModel: SplitBillViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
+    state: SplitBillState,
+    onEvent: (SplitBillEvent) -> Unit
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+    fun isNotEmpty(list: List<*>?): Boolean {
+        return list != null && list.isNotEmpty()
+    }
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -63,12 +73,16 @@ fun SplitBillScreen(
                         )
                         Spacer(modifier = Modifier.height(16.dp))
                         CustomOutlinedTextFields(
-                            value = viewModel.bill.collectAsState().value,
+                            value = state.bill,
                             onNewValue = {
-                                viewModel.setBill(it)
+                                if(it.length <= state.maxBillDigits){
+                                    onEvent(SplitBillEvent.SetBill(it))
+                                } else {
+                                    onEvent(SplitBillEvent.ExceedMaxDigits)
+                                }
                             },
                             hint = " 000",
-                            isError = viewModel.emptyBill.collectAsState().value,
+                            isError = state.errorBillInput,
                             modifier = Modifier.fillMaxSize(),
                             leadingIcon = {
                                 Text(
@@ -79,7 +93,10 @@ fun SplitBillScreen(
                                     )
                                 )
                             },
-                            errorMessage = viewModel.errorMessage.collectAsState().value
+                            errorMessage = when(state.errorBillInput){
+                                true -> state.errorMessage
+                                else -> "please do not use '.' or ',' i'm still working on currency formatting :D"
+                            }
                         )
                         Spacer(modifier = Modifier.height(16.dp))
                         Text(
@@ -90,12 +107,12 @@ fun SplitBillScreen(
                         )
                         Spacer(modifier = Modifier.height(16.dp))
                         CustomOutlinedTextFields(
-                            value = viewModel.person.collectAsState().value,
+                            value = state.person,
                             onNewValue = {
-                                viewModel.setPerson(it)
+                                onEvent(SplitBillEvent.SetPerson(it))
                             },
                             hint = "0",
-                            isError = viewModel.emptyPerson.collectAsState().value,
+                            isError = state.errorPersonInput,
                             modifier = Modifier.fillMaxSize(),
                             leadingIcon = {
                                 Icon(
@@ -103,30 +120,46 @@ fun SplitBillScreen(
                                     contentDescription = "person"
                                 )
                             },
-                            errorMessage = viewModel.errorMessage.collectAsState().value
+                            errorMessage = if(state.errorPersonInput){
+                                state.errorMessage
+                            } else ""
                         )
                         Spacer(modifier = Modifier.height(16.dp))
                     }
                     item {
                         Row(
-                            verticalAlignment = Alignment.CenterVertically
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.padding(horizontal = 8.dp)
                         ) {
-                            if (viewModel.splittedBill.collectAsState().value != "") {
-                                Text(
-                                    text = "Splitted bill: Rp.${viewModel.splittedBill.collectAsState().value}",
-                                    modifier = Modifier.weight(
-                                        1f
+                            if (state.finalBill != "") {
+                                Card(
+                                    colors = CardDefaults.cardColors(
+                                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                        contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                                    ), modifier = Modifier
+                                        .weight(
+                                            1f
+                                        )
+                                        .padding(end = 4.dp)
+                                        .fillMaxHeight()
+                                ) {
+                                    Text(
+                                        text = "Splitted bill: Rp.${state.finalBill}",
+                                        modifier = Modifier.padding(16.dp)
                                     )
-                                )
+                                }
                             }
-                            if(viewModel.splittedBill.collectAsState().value != ""){
+                            if (state.finalBill != "") {
                                 Button(
                                     onClick = {
-                                        viewModel.clearState()
+                                        onEvent(SplitBillEvent.ClearState)
                                     },
                                     content = {
                                         Text("Clear")
-                                    }, modifier = Modifier.weight(1f), colors = ButtonDefaults.buttonColors(
+                                    },
+                                    modifier = Modifier.weight(1f)
+                                        .padding(start = 8.dp),
+                                    colors = ButtonDefaults.buttonColors(
                                         containerColor = MaterialTheme.colorScheme.error,
                                         contentColor = MaterialTheme.colorScheme.onError
                                     )
@@ -134,17 +167,41 @@ fun SplitBillScreen(
                             } else {
                                 Button(
                                     onClick = {
-                                        viewModel.splitTheBill(
-                                            bill = viewModel.bill.value,
-                                            person = viewModel.person.value
-                                        )
+                                        onEvent(SplitBillEvent.SaveSplitBill)
                                     },
                                     content = {
                                         Text("Split!")
-                                    }, modifier = Modifier.weight(1f)
+                                    },
+                                    modifier = Modifier.weight(1f)
                                 )
                             }
                         }
+                    }
+                    item {
+                        if (isNotEmpty(state.bills)) {
+                            Text(
+                                text = "History", style = TextStyle(
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                ),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 16.dp, horizontal = 8.dp)
+                            )
+                        }
+                    }
+                    items(state.bills) {
+                        BillsItem(
+                            bills = it,
+                            onClick = {
+                                onEvent(
+                                    SplitBillEvent.DeleteSplitBill(
+                                        it
+                                    )
+                                )
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        )
                     }
                 },
                 contentPadding = PaddingValues(
