7eb0310f06d0fd85f1e8115a514c6431c7641cf6
Sendiko <sarangtawon897@gmail.com>
2023-11-27 13:14:48 +0700

    enc(crea): app preference class

---
+++ app/src/main/java/com/sendiko/split_the_bill/repository/AppPreferences.kt
@@ -0,0 +1,38 @@
+package com.sendiko.split_the_bill.repository
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class AppPreferences private constructor(private val dataStore: DataStore<Preferences>){
+
+    private val darkThemeKey = booleanPreferencesKey("is_dark")
+
+    fun getDarkTheme(): Flow<Boolean> {
+        return dataStore.data.map {
+            it[darkThemeKey]?:false
+        }
+    }
+
+    suspend fun setDarkTheme(isDarkTheme: Boolean){
+        dataStore.edit {
+            it[darkThemeKey] = isDarkTheme
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppPreferences? = null
+
+        fun getInstance(dataStore: DataStore<Preferences>): AppPreferences {
+            return INSTANCE ?: synchronized(this) {
+                val instance = AppPreferences(dataStore)
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
