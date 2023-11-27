b2cf2e2000d99d2efc58bd2bf6181d97c75978cf
Sendiko <sarangtawon897@gmail.com>
2023-11-27 13:21:47 +0700

    enc(add): AppPreferences to ViewModelFactory

--- app/src/main/java/com/sendiko/split_the_bill/repository/viewmodels/ViewModelFactory.kt
+++ app/src/main/java/com/sendiko/split_the_bill/repository/viewmodels/ViewModelFactory.kt
@@ -3,19 +3,23 @@ package com.sendiko.split_the_bill.repository.viewmodels
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.sendiko.split_the_bill.repository.AppPreferences
 
-class ViewModelFactory private constructor(private val app : Application): ViewModelProvider.NewInstanceFactory(){
+class ViewModelFactory private constructor(
+    private val app: Application,
+    private val pref: AppPreferences
+): ViewModelProvider.NewInstanceFactory(){
 
     companion object {
         @Volatile
         private var INSTANCE: ViewModelFactory? = null
 
         @JvmStatic
-        fun getInstance(application: Application): ViewModelFactory {
+        fun getInstance(application: Application, preferences: AppPreferences): ViewModelFactory {
             when (INSTANCE) {
                 null -> {
                     synchronized(ViewModelFactory::class.java) {
-                        INSTANCE = ViewModelFactory(application)
+                        INSTANCE = ViewModelFactory(application, preferences)
                     }
                 }
             }
@@ -25,7 +29,7 @@ class ViewModelFactory private constructor(private val app : Application): ViewM
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
-            modelClass.isAssignableFrom(SplitBillViewModel::class.java)-> SplitBillViewModel(app) as T
+            modelClass.isAssignableFrom(SplitBillViewModel::class.java)-> SplitBillViewModel(app, pref) as T
             else -> throw IllegalArgumentException("Unknown modelclass: " + modelClass.name)
         }
     }
