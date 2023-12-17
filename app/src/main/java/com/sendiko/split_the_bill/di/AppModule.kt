d69834033aa2fa1da99f3b165466b66f2fc563ab
Sendiko <rizkysendiko7@gmail.com>
2023-12-17 17:46:35 +0700

    enc(crea): AppModule to provide dependencies

---
+++ app/src/main/java/com/sendiko/split_the_bill/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.sendiko.split_the_bill.di
+
+import android.content.Context
+import androidx.room.Room
+import com.sendiko.split_the_bill.repository.database.BillDao
+import com.sendiko.split_the_bill.repository.database.Database
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context): Database {
+        return Room.databaseBuilder(
+            context = context,
+            klass = Database::class.java,
+            name = "splitbill.db"
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideDao(database: Database): BillDao{
+        return database.dao
+    }
+}
