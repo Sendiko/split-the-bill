2e32af32b26cc39cdbc2d450008acb75ec11662f
Sendiko <rizkysendiko7@gmail.com>
2023-12-17 17:50:16 +0700

    enc(imp): AndroidEntryPoint and viewmodel declaration

--- app/src/main/java/com/sendiko/split_the_bill/MainActivity.kt
+++ app/src/main/java/com/sendiko/split_the_bill/MainActivity.kt
@@ -1,33 +1,28 @@
 package com.sendiko.split_the_bill
 
-import android.app.Application
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.lifecycle.ViewModelProvider
-import com.sendiko.split_the_bill.repository.viewmodels.SplitBillViewModel
-import com.sendiko.split_the_bill.repository.viewmodels.ViewModelFactory
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.sendiko.split_the_bill.ui.screen.SplitBillScreen
+import com.sendiko.split_the_bill.ui.screen.SplitBillViewModel
 import com.sendiko.split_the_bill.ui.theme.SplitthebillTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private fun obtainViewModel(application: Application): SplitBillViewModel{
-        val factory = ViewModelFactory.getInstance(application)
-        return ViewModelProvider(this, factory)[SplitBillViewModel::class.java]
-    }
-
-    private val viewModel: SplitBillViewModel by lazy {
-        obtainViewModel(requireNotNull(application))
-    }
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
+            val viewModel = hiltViewModel<SplitBillViewModel>()
             SplitthebillTheme {
                 val state by viewModel.state.collectAsState()
                 SplitBillScreen(state = state, onEvent = viewModel::onEvent)
